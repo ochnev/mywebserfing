7deb2a9fbe080c5a96896ce3f169eb4f07b0ebd1
Dmiry O <itpro2012java@gmail.com>
2021-11-06 14:41:16 +0200

    Added an alternative test for the home page controller

---
+++ src/test/java/com/mywebsurfing/controller/HomePageControllerRestTemplateTest.java
@@ -0,0 +1,34 @@
+package com.mywebsurfing.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class HomePageControllerRestTemplateTest {
+
+    @LocalServerPort
+    private int port;
+
+    private String url;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @BeforeEach
+    public void setUp() {
+        url = String.format("http://localhost:%d/home", port);
+    }
+
+    @Test
+    public void greetingShouldReturnDefaultMessage() {
+        String contents = this.restTemplate.getForObject(url, String.class);
+        assertTrue(contents.contains("IT"));
+    }
+
+}
