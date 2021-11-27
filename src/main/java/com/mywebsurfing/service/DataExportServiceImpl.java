3213505232e4ebdea2bc7525062fc46e0ca51c74
Dmiry O <itpro2012java@gmail.com>
2021-11-27 15:14:33 +0200

    Refactored DataExportServiceImpl, added export of LinkCollections

--- src/main/java/com/mywebsurfing/service/DataExportServiceImpl.java
+++ src/main/java/com/mywebsurfing/service/DataExportServiceImpl.java
@@ -19,6 +19,12 @@ public class DataExportServiceImpl implements DataExportService {
 
     @Override
     public String exportData(AppUser user) {
+        List<String> bookmarkRows = exportBookmarks(user);
+        List<String> linkCollectionRows = exportLinkCollections(user);
+        return String.join("\n", bookmarkRows) + "\n" + String.join("\n", linkCollectionRows);
+    }
+
+    private List<String> exportBookmarks(AppUser user) {
         String queryText = "SELECT r.name, f.name, b.url, b.title " +
                 "FROM realm r JOIN folder f ON f.realm_id = r.id " +
                 "JOIN bookmark b ON b.folder_id = f.id " +
@@ -36,8 +42,26 @@ public class DataExportServiceImpl implements DataExportService {
                             return String.join(";", Arrays.asList(realmName, folderName, linkUrl, linkTitle));
                         })
                 .stream().collect(Collectors.toList());
+        return rows;
+    }
 
-        return String.join("\n", rows);
+    private List<String> exportLinkCollections(AppUser user) {
+        String queryText = "SELECT r.name, lc.url, lc.name " +
+                "FROM realm r JOIN link_collection lc ON lc.realm_id = r.id " +
+                "WHERE r.app_user_id = ? " +
+                "ORDER BY r.id ASC";
+        Object[] args = new Object[] { user.getId() };
+        int[] argTypes = new int[] { Types.BIGINT };
+
+        List<String> rows = jdbcTemplate.query(queryText, args, argTypes,
+                        (rs, rowNum) -> {
+                            String realmName = rs.getString(1);
+                            String lcUrl = rs.getString(2);
+                            String lcName = rs.getString(3);
+                            return String.join(";", Arrays.asList(realmName, "", lcUrl, lcName));
+                        })
+                .stream().collect(Collectors.toList());
+        return rows;
     }
 
 }
